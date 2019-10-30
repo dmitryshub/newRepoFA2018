47fe0cc13e46ef568ddb2cbc7e5e82a1682297d3
dmitryshub <dshub@colum.edu>
2019-10-30 17:42:46 -0500

    Add files via upload

---
+++ IncrementDemo/IncrementDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncrementDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            a = a + 10;
+
+            Console.WriteLine(a);
+            Console.Read();
+        }
+    }
+}
